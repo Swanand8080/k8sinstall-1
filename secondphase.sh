30b3c74cb919cc5db676f1a50f2a3a40bc4ca870
ashutoshbhakare <bhakareashutosh@yahoo.com>
2022-03-08 17:00:16 +0530

    Create secondphase.sh

---
+++ secondphase.sh
@@ -0,0 +1,19 @@
+#please run this on master only
+sudo cat <<EOF | sudo tee /etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+sudo systemctl enable docker
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+sudo kubeadm init
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+kubectl get nodes 
