31a9794ee9bf2e3a0d76c6bf9788e7d9db890899
ashutoshbhakare <bhakareashutosh@yahoo.com>
2022-03-08 16:34:33 +0530

    Update basescript.sh

--- basescript.sh
+++ basescript.sh
@@ -1,27 +1,26 @@
 cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
-ilter
-
+br_netfilter
+EOF
 cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
-dge.bridge-nf-call-ip6tables = 1
-dge.bridge-nf-call-iptables = 1
-
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
 sudo sysctl --system
 curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh 
+sudo sh get-docker.sh
 cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
-etes]
-bernetes
-=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
-=1
-k=1
-gcheck=1
-https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.c
-loud.google.com/yum/doc/rpm-package-key.gpg
-=kubelet kubeadm kubectl
-
-
-getenforce 
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/d
+oc/rpm-package-key.gpg
+exclude=kubelet kubeadm kubectl
+EOF
+getenforce
 sudo setenforce 0
-sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/con
-fig
+sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
 sudo yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+sudo systemctl enable --now kubelet
