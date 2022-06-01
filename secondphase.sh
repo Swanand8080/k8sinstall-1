6e79c4f90b5574359cbfaacc2f9e387a4e7324fc
ashutoshbhakare <bhakareashutosh@yahoo.com>
2022-06-01 09:00:44 +0530

    Update secondphase.sh

--- secondphase.sh
+++ secondphase.sh
@@ -12,6 +12,8 @@ EOF
 sudo systemctl enable docker
 sudo systemctl daemon-reload
 sudo systemctl restart docker
+yum remove kubeadm kubelet kubectl 
+yum install kubeadm-1.23.1-0.x86_64 kubelet-1.23.1-0.x86_64 kubectl-1.23.1-0.x86_64 --disableexcludes=kubernetes -y
 sudo kubeadm init
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
